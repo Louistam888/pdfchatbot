8fe34c70bc3ddcf66527bb51e9f4a2cec4724ae8
Louistam888 <louistam888@gmail.com>
2025-03-22 21:09:06 -0400

    added additoinal components

---
+++ components/ui/chatbubble.tsx
@@ -0,0 +1,24 @@
+import Balancer from "react-wrap-balancer";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./accordion";
+import { Message } from "ai/react";
+
+type Props = {};
+
+const chatbubble = (props: Props) => {
+  return <div>chatbubble</div>;
+};
+
+export default chatbubble;
