02e0a25ab41e02717cac86f9f96cb5edb368a301
Louistam888 <louistam888@gmail.com>
2025-04-04 19:39:45 -0400

    pinecone initliaized i thinik

--- lib/pinecone-client.ts
+++ lib/pinecone-client.ts
@@ -1,14 +1,24 @@
-import { Pinecone } from "@pinecone-database/pinecone";
+import {
+  Pinecone,
+  type PineconeConfiguration,
+} from "@pinecone-database/pinecone";
 import { env } from "./config";
+import { delay } from "./utils";
 
 const pineconeClientInstance = new Pinecone({
   apiKey: env.PINECONE_API_KEY,
 });
 
-// const initPineconeClient = async () => {
-//   const pineconeClient = await pineconeClientInstance.init({
-//     apiKey: env.PINECONE_API_KEY,
-//     environment: env.PINECONE_ENVIRONMENT,
-//   });
-// };
- 
+await pineconeClientInstance.createIndex({
+  name: "sample-index",
+  dimension: 1536,
+  metric: "cosine",
+  spec: {
+    serverless: {
+      cloud: "gcp",
+      region: env.PINECONE_ENVIRONMENT,
+    },
+  },
+  tags: { team: "data-science" },
+});
+
