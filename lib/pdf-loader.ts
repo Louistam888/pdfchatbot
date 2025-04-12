12391d8700f686e218c2f6b26c3a7b08fb52facb
Louistam888 <louistam888@gmail.com>
2025-04-12 13:20:45 -0400

    creating vectore store

--- lib/pdf-loader.ts
+++ lib/pdf-loader.ts
@@ -1,3 +1,20 @@
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { env } from "./config";
+
+export const getChunkedDocsFromPDF = async () => {
+  try {
+    const loader = new PDFLoader(env.PDF_PATH);
+    const docs = await loader.load();
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+
+    const chunkedDocs = await textSplitter.splitDocuments(docs);
+    return chunkedDocs;
+  } catch (error) {
+    console.error("Error loading PDF:", error);
+    throw new Error("PDF docs chunking failed");
+  }
+};
